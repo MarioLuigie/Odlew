308f57ecba71a16591a5b0f7d8e236858d3fe0af
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-29 18:24:08 +0100

    Refactor: Rebuild HomePage

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -4,11 +4,12 @@ import CustomButton from '@/components/shared/CustomButton'
 
 export default function HomePage() {
 	return (
-		<div className="h-homePage p-6 flex items-end homepage-background">
-			<div className='flex flex-col gap-3 pb-12'>
+		<div className="flex flex-col">
+			<div className="h-homePage flex flex-col justify-end gap-4 p-4 pb-12 homepage-background">
 				<CompanyName mode="light" className="h-auto" />
 				<CustomButton />
 			</div>
+			<div className="bg-red-300 w-full h-[120px]">SECTION</div>
 		</div>
 	)
 }
