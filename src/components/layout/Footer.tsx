2b22bc2bffbfea58477d853df44ad720821ce346
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-10-31 11:37:37 +0100

    Styles: Add max width for MenuFooter in Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
 		<footer className="flex flex-col items-center justify-center bg-footer text-white px-10 py-12">
 			<div className="flex flex-col items-center gap-12 w-full">
 				<Logo variant="mono" w={270} />
-				<div className="flex justify-center w-full">
+				<div className="flex justify-center w-full max-w-[350px]">
 					<div className="w-[2px] bg-accent"></div>
 					<MenuFooter />
 				</div>
