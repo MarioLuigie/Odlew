4eced39cfaa377f93896b9bd356b4a8e3527543b
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-11-02 03:46:18 +0100

    Fix: Add click effect on entire Link in CustomButton

--- src/components/shared/CustomButton.tsx
+++ src/components/shared/CustomButton.tsx
@@ -1,48 +1,52 @@
-'use client'
+"use client"
 // modules
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import { cn } from '@/lib/utils'
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
 
 type CustomButtonProps = {
-	children: React.ReactNode
-	onClick?: () => void
-	navlink?: boolean
-	path?: string
-	className?: string
-	disabled?: boolean
+  children: React.ReactNode
+  onClick?: () => void
+  navlink?: boolean
+  path?: string
+  className?: string
+  disabled?: boolean
 }
 
 export default function CustomButton({
-	children,
-	onClick,
-	navlink,
-	path,
-	className,
-	disabled=false,
+  children,
+  onClick,
+  navlink,
+  path,
+  className,
+  disabled = false,
 }: CustomButtonProps) {
-	const buttonClasses =
-		'bg-accent text-md text-textLight w-full rounded-l-full rounded-br-full py-6 hover:bg-accentHover'
+  const buttonClasses =
+    "bg-accent text-md text-textLight w-full rounded-l-full rounded-br-full py-6 hover:bg-accentHover"
 
-	if (navlink && path) {
-		return (
-			<div
-				className={cn(
-					'h-0 flex justify-center items-center',
-					buttonClasses,
-					className
-				)}
-			>
-				<Link href={path} {...(onClick && { onClick: onClick })}>
-					{children}
-				</Link>
-			</div>
-		)
-	}
+  if (navlink && path) {
+    return (
+      <Link
+        href={path}
+        {...(onClick && { onClick: onClick })}
+        className={cn(
+          "h-0 flex justify-center items-center",
+          buttonClasses,
+          className
+        )}
+      >
+        {children}
+      </Link>
+    )
+  }
 
-	return (
-		<Button onClick={onClick} className={cn(buttonClasses, className)} disabled={disabled}>
-			{children}
-		</Button>
-	)
+  return (
+    <Button
+      onClick={onClick}
+      className={cn(buttonClasses, className)}
+      disabled={disabled}
+    >
+      {children}
+    </Button>
+  )
 }
