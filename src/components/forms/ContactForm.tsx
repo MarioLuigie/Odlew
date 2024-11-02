7e13592e6f04aede4f991c650aaf95f805fbc5ee
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-11-02 01:39:08 +0100

    Refactor: Comment imports in ContactForm

--- src/components/forms/ContactForm.tsx
+++ src/components/forms/ContactForm.tsx
@@ -1,8 +1,8 @@
 'use client'
 // modules
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { useRouter, usePathname } from 'next/navigation'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useForm, SubmitHandler } from 'react-hook-form'
+// import { useRouter, usePathname } from 'next/navigation'
 
 export default function ContactForm() {
   return (
