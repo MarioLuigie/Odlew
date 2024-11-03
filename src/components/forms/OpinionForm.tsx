d8db3c097008f5cc021004b01fe031b352e527ec
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-11-03 23:02:58 +0100

    Feat: Create OpinionForm and OpinionFormSchema in zod.ts

---
+++ src/components/forms/OpinionForm.tsx
@@ -0,0 +1,7 @@
+export default function OpinionForm() {
+  return (
+    <div>
+      Opinion Form
+    </div>
+  )
+}
--- src/lib/types/zod.ts
+++ src/lib/types/zod.ts
@@ -15,7 +15,23 @@ export const ContactFormSchema = z.object({
   message: z
     .string()
     .min(40, "Wiadomość musi zawierać min. 40 znaków")
-    .max(500, "Wiadomość musi zawierać min. 500 znaków"),
+    .max(500, "Wiadomość może zawierać max. 500 znaków"),
 })
 
 export type ContactFormValues = z.infer<typeof ContactFormSchema>
+
+// OpinionForm Schema
+export const OpinionFormSchema = z.object({
+  name: z
+  .string()
+  .min(5, "Imię musi zawierać min. 5 znaków")
+  .max(40, "Imie może zawierać max. 40 znaków"),
+  stars: z
+  .number(),
+  message: z
+  .string()
+  .min(40, "Opinia musi zawierać min. 5 znaków")
+  .max(500, "Opinia może zawierać max. 200 znaków"),
+})
+
+export type OpinionFormValues = z.infer<typeof OpinionFormSchema>
