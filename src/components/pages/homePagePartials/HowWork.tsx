db5a0ec62aa32b2c74bf636397b9ee291d0600a7
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-11-07 16:26:38 +0100

    Feat: Add HowWorkContent to HowWork.tsx

--- src/components/pages/homePagePartials/HowWork.tsx
+++ src/components/pages/homePagePartials/HowWork.tsx
@@ -1,19 +1,28 @@
 'use client'
 // modules
-// import { useState } from 'react'
+import { useState } from 'react'
+// lib
+import { cn } from '@/lib/utils'
+import { HowWorkContent } from '@/lib/constants/texts'
 // components
 import Title from '@/components/shared/Title'
 
 export default function HowWork() {
-	// const [activContentIndex, setActiveContentIndex] = useState<number>(0)
+	const [activContentIndex, setActiveContentIndex] = useState<number>(0)
 
 	return (
 		<div id="tabs" className="min-h-customScreen mb-4">
 			<Title className="mb-6 px-5">
 				<p>Jak pracujemy?</p>
 			</Title>
-			<menu className="w-full px-5 pb-8 flex flex-col items-center text-textDark">
-				<button className="relative w-full py-2 px-4 rounded-l-full rounded-br-full border-2 border-accent text-center">
+			<menu className="w-full px-5 pb-10 flex flex-col items-center text-textDark">
+				<button
+					onClick={() => setActiveContentIndex(0)}
+					className={cn(
+						'relative w-full py-2 px-4 rounded-l-full rounded-br-full border-2 border-accent text-center',
+						activContentIndex === 0 ? 'bg-accent text-textLight' : ''
+					)}
+				>
 					<p>Kontakt z klientem</p>
 					<div className="h-full aspect-square absolute flex justify-center items-center rounded-full top-0 left-0 bg-accent text-textLight">
 						1
@@ -21,7 +30,13 @@ export default function HowWork() {
 				</button>
 				<div className="w-[2px] h-[30px] bg-accent" />
 
-				<button className="relative w-full py-2 px-4 rounded-l-full rounded-br-full border-2 border-accent text-center">
+				<button
+					onClick={() => setActiveContentIndex(1)}
+					className={cn(
+						'relative w-full py-2 px-4 rounded-l-full rounded-br-full border-2 border-accent text-center',
+						activContentIndex === 1 ? 'bg-accent text-textLight' : ''
+					)}
+				>
 					<p>Tworzenie projektu</p>
 					<div className="h-full aspect-square absolute flex justify-center items-center rounded-full top-0 left-0 bg-accent text-textLight">
 						2
@@ -29,7 +44,13 @@ export default function HowWork() {
 				</button>
 				<div className="w-[2px] h-[30px] bg-accent" />
 
-				<button className="relative w-full py-2 px-4 rounded-l-full rounded-br-full border-2 border-accent text-center">
+				<button
+					onClick={() => setActiveContentIndex(2)}
+					className={cn(
+						'relative w-full py-2 px-4 rounded-l-full rounded-br-full border-2 border-accent text-center',
+						activContentIndex === 2 ? 'bg-accent text-textLight' : ''
+					)}
+				>
 					<p>Realizacja projektu</p>
 					<div className="h-full aspect-square absolute flex justify-center items-center rounded-full top-0 left-0 bg-accent text-textLight">
 						3
@@ -37,14 +58,22 @@ export default function HowWork() {
 				</button>
 				<div className="w-[2px] h-[30px] bg-accent" />
 
-				<button className="relative w-full py-2 px-4 rounded-l-full rounded-br-full border-2 border-accent text-center">
+				<button
+					onClick={() => setActiveContentIndex(3)}
+					className={cn(
+						'relative w-full py-2 px-4 rounded-l-full rounded-br-full border-2 border-accent text-center',
+						activContentIndex === 3 ? 'bg-accent text-textLight' : ''
+					)}
+				>
 					<p>Odbiór realizacji</p>
 					<div className="h-full aspect-square absolute flex justify-center items-center rounded-full top-0 left-0 bg-accent text-textLight">
 						4
 					</div>
 				</button>
 			</menu>
-			<div className="w-full h-[400px] bg-mottoBg"></div>
+			<div id="tab-content" className="w-full min-h-[200px] bg-mottoBg px-5 py-8">
+					<p className='text-justify'>{HowWorkContent[activContentIndex]}</p>
+			</div>
 		</div>
 	)
 }
--- src/lib/constants/texts.ts
+++ src/lib/constants/texts.ts
@@ -1,32 +1,40 @@
 export const IconsAlt = {
-  MENU: 'Menu',
-  CLOSE: 'Zamykanie',
-  ITEM: 'Element',
+	MENU: 'Menu',
+	CLOSE: 'Zamykanie',
+	ITEM: 'Element',
 }
 
 export const ImagesAlt = {
-  LOGO: 'Logo',
-  ABOUT: 'Zdjęcie o nas',
-  OFFER: 'Zdjęcie oferta',
-  PRODUCT: 'Zdjęcie produktu',
+	LOGO: 'Logo',
+	ABOUT: 'Zdjęcie o nas',
+	OFFER: 'Zdjęcie oferta',
+	PRODUCT: 'Zdjęcie produktu',
 }
 
 export const Menu = {
-  HOME: 'strona główna',
-  ABOUT: 'o nas',
-  OFFER: 'oferta',
-  REALIZATIONS: 'realizacje',
-  HOW_WORK: 'jak pracujemy',
-  CONTACT: 'kontakt',
-  POLICY: 'polityka prywatności',
+	HOME: 'strona główna',
+	ABOUT: 'o nas',
+	OFFER: 'oferta',
+	REALIZATIONS: 'realizacje',
+	HOW_WORK: 'jak pracujemy',
+	CONTACT: 'kontakt',
+	POLICY: 'polityka prywatności',
 }
 
 export const App = {
-  NAME: 'P.S. Odlew',
-  SUBNAME: 'Odlewnia Artystyczna',
-  ADDRESS: 'Jawornik 505, Myślenice',
-  EMAIL: 'polewka.szymon@gmail.com',
-  PHONE: '+48 665 981 994',
+	NAME: 'P.S. Odlew',
+	SUBNAME: 'Odlewnia Artystyczna',
+	ADDRESS: 'Jawornik 505, Myślenice',
+	EMAIL: 'polewka.szymon@gmail.com',
+	PHONE: '+48 665 981 994',
 }
 
+export const HowWorkContent = [
+	'Podczas kontaktu, dokładnie omawiamy potrzeby klienta i zakres prac. Prosimy o przesłanie kompletnych informacji na temat projektu m.in. wymiary, zdjęcia, zawartość. Na podstawie otrzymanych informacji dokonujemy wyceny oraz określamy przybliżony termin realizacji.',
 
+	'Przedstawiamy wykonany przez nas projekt, który poddajemy akceptacji zamawiającemu.',
+
+	'Po uzyskaniu akceptacji projektu przystępujemy do realizacji zamówienia z największą starannością. Na każdym etapie realizacji utrzymujemy stały kontakt z klientem.',
+
+	'Gotowy odlew prezentujemy klientowi podczas osobistego odbioru. Oferujemy również wysyłkę kurierską mniejszych realizacji jak również transport większych realizacji.',
+]
