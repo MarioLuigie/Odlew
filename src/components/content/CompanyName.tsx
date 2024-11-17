416edab8caa68592fd89046db0e4fa3d18ed5df2
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-11-17 14:05:04 +0100

    Refactor

--- src/components/content/CompanyName.tsx
+++ src/components/content/CompanyName.tsx
@@ -1,20 +1,19 @@
 // lib
-import { App } from "@/lib/constants/texts"
-import { cn } from "@/lib/utils"
+import { App } from '@/lib/constants/texts'
+import { cn } from '@/lib/utils'
 // components
-import TextVerticalLine from "@/components/shared/TextVerticalLine"
+import TextVerticalLine from '@/components/shared/TextVerticalLine'
 
-export default function CompanyName({ mode }: { mode: "dark" | "light" }) {
-  const textStyles = "text-[21px] text-right"
-  const textVariants = {
-    dark: "text-textDark",
-    light: "text-textLight",
-  }
-
-  return (
-    <TextVerticalLine className="text-right h-auto" mode={mode}>
-      <p className={cn(textStyles, textVariants[mode])}>{App.NAME}</p>
-      <p className={cn(textStyles, textVariants[mode])}>{App.SUBNAME}</p>
-    </TextVerticalLine>
-  )
+export default function CompanyName({ mode }: { mode: 'dark' | 'light' }) {
+	const textStyles = 'text-[21px] text-right'
+	const textVariants = {
+		dark: 'text-textDark',
+		light: 'text-textLight',
+	}
+	return (
+		<TextVerticalLine className="text-right h-auto" mode={mode}>
+			<p className={cn(textStyles, textVariants[mode])}>{App.NAME}</p>
+			<p className={cn(textStyles, textVariants[mode])}>{App.SUBNAME}</p>
+		</TextVerticalLine>
+	)
 }
