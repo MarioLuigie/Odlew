b35c20cce96e4d3225049015c082845186d6c9c8
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-11-19 18:07:47 +0100

    Feat: Add children to Drop.tsx and style them

--- src/components/shared/Drop.tsx
+++ src/components/shared/Drop.tsx
@@ -1,54 +1,75 @@
 // modules
-import { cva } from 'class-variance-authority';
+import { cva } from 'class-variance-authority'
 // lib
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils'
 
 type DropProps = {
-  corner?: 'bottom-right' | 'bottom-left' | 'top-left' | 'top-right';
-  className?: string;
-  text?: string;
-  variant?: 'outline' | 'fill';
-  color?: string;
-};
-
-export default function Drop({ corner, className, text, variant, color }: DropProps) {
-  const dropVariants = cva(
-    'w-[35px] h-[35px] flex justify-center items-center rounded-full', 
-    {
-      variants: {
-        corner: {
-          'bottom-left': 'rounded-bl-none',
-          'bottom-right': 'rounded-br-none',
-          'top-left': 'rounded-tl-none',
-          'top-right': 'rounded-tr-none',
-        },
-        variant: {
-          outline: 'bg-transparent border-2 border-accent',
-          fill: 'bg-accent',
-        },
-      },
-      defaultVariants: {
-        corner: 'bottom-left',
-        variant: 'fill',
-      },
-    }
-  );
-
-  const style = color
-    ? {
-        backgroundColor: variant === 'outline' ? 'transparent' : color,
-        borderColor: variant === 'outline' ? color : 'transparent',
-      }
-    : undefined;
-
-  return (
-    <div className={cn(className, dropVariants({ corner, variant }))} style={style}>
-      {text && <p className="text-[20px] text-textLight">{text}</p>}
-    </div>
-  );
+	corner?: 'bottom-right' | 'bottom-left' | 'top-left' | 'top-right'
+	className?: string
+	variant?: 'outline' | 'fill'
+	color?: string
+	children?: React.ReactNode
 }
 
+export default function Drop({
+	corner = 'bottom-left',
+	className,
+	variant = 'fill',
+	color,
+	children,
+}: DropProps) {
+	const dropVariants = cva(
+		'w-[35px] h-[35px] flex justify-center items-center rounded-full',
+		{
+			variants: {
+				corner: {
+					'bottom-left': 'rounded-bl-none',
+					'bottom-right': 'rounded-br-none',
+					'top-left': 'rounded-tl-none',
+					'top-right': 'rounded-tr-none',
+				},
+				variant: {
+					outline: 'bg-transparent border-2 border-accent',
+					fill: 'bg-accent',
+				},
+			},
+			defaultVariants: {
+				corner: 'bottom-left',
+				variant: 'fill',
+			},
+		}
+	)
 
+	const containerStyle = color
+		? {
+				backgroundColor: variant === 'outline' ? 'transparent' : color,
+				borderColor: variant === 'outline' ? color : 'transparent',
+		  }
+		: undefined
+
+	const textStyle = color
+		? variant === 'outline'
+			? { color }
+			: { color: 'white' }
+		: undefined
+
+	return (
+		<div
+			className={cn(className, dropVariants({ corner, variant }))}
+			style={containerStyle}
+		>
+			<div
+				className={cn(
+					'text-accent',
+					variant === 'fill' && 'text-textLight'
+				)}
+				style={textStyle}
+			>
+				{children}
+			</div>
+		</div>
+	)
+}
 
 // // modules
 // import { cva } from 'class-variance-authority'
--- src/components/shared/Test.tsx
+++ src/components/shared/Test.tsx
@@ -1,16 +1,19 @@
 'use client'
 import { toastSuccess } from '@/lib/utils/toasts'
 import Drop from './Drop'
+import { MoveDownRight } from 'lucide-react'
 
 export default function Test() {
 	return (
 		<div className="bg-zinc-400 min-h-[200px] p-4 pb-8 flex flex-col relative">
-			<small className="mb-2 p-2 text-zinc-600">
+			<small className="p-2 text-zinc-600">
 				Console Only for developers!
 			</small>
-			<p className="bg-zinc-200 rounded-sm p-2">Actions:</p>
+
+			{/* ACTIONS */}
+			<p className="bg-zinc-200 rounded-sm p-2 mt-2">Actions:</p>
 			<div className="p-2 mt-2 rounded-md">
-				<div className="py-4 px-2 flex gap-3 items-center border-b-[1px] border-zinc-500">
+				<div className="py-4 px-2 flex gap-3 items-center">
 					<p>call for a toast</p>
 					<button
 						className="py-2 px-4 bg-blue-400 border-[1px] border-blue-800 rounded-sm"
@@ -24,10 +27,17 @@ export default function Test() {
 						TOAST
 					</button>
 				</div>
-				<div>
-					<Drop variant="outline" color='#d1f141' corner='bottom-right' />
-				</div>
 			</div>
+
+			{/* COMPONENTS */}
+			<p className="bg-zinc-200 rounded-sm p-2 mt-2">Components:</p>
+			<div className="p-4 flex gap-3 items-center">
+				<p>Drop.tsx</p>
+				<Drop variant="outline" color='#eecd13' corner="bottom-right">
+					<MoveDownRight />
+				</Drop>
+			</div>
+
 			<small className="text-zinc-600 py-2 absolute bottom-0 right-0 w-full flex justify-center items-center">
 				&copy; ARWcode 2024. All rights reserved.
 			</small>
