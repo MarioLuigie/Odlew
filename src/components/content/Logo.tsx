a8eea73390bce90b840da861f661b88e4e859afb
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-12-02 20:06:00 +0100

    Feat: Add container with company name in Start.tsx only for desktop ver

--- src/components/content/Logo.tsx
+++ src/components/content/Logo.tsx
@@ -27,7 +27,7 @@ export default function Logo({
 				width={w}
 				alt={imageAlt}
 				priority
-				className="hidden dsktp:block"
+				className="hidden dp:block"
 			/>
 			<Image
 				src={mobileImageSrc}
@@ -35,7 +35,7 @@ export default function Logo({
 				width={50}
 				alt={imageAlt}
 				priority
-				className="dsktp:hidden"
+				className="dp:hidden"
 			/>
 		</Link>
 	)
--- src/components/pages/homePagePartials/Start.tsx
+++ src/components/pages/homePagePartials/Start.tsx
@@ -3,25 +3,30 @@ import CustomButton from '@/components/shared/CustomButton'
 
 export default function Start() {
 	return (
-		<div
-			id="home"
-			className="h-customScreen p-5 dp:px-10 pb-28 flex flex-col justify-end dp:justify-end dp:items-start gap-4 homepage-background scroll-mt-header"
-		>
-			<CompanyName
-				mode="light"
-				className="dp:hidden"
-			/>
-      <div className='hidden dp:text-textLight dp:flex dp:flex-col'>
-        <p className='text-[50px]'>P.S Odlew</p>
-        <p className='text-[95px]'>Odlewnia Artystyczna</p>
-      </div>
-			<CustomButton
-				navlink
-				path="/realizacje"
-				className="dp:max-w-[240px]"
+		<div className="w-full dp:flex">
+			{/* DESKTOP */}
+			<div className="hidden dp:flex dp:flex-col dp:justify-center dp:w-1/2 dp:bg-mottoBg dp:p-8">
+				<div className="hidden dp:text-textDark dp:flex dp:flex-col">
+					<p className="text-[30px] xl:text-[45px]">P.S Odlew</p>
+					<p className="text-[65px] xl:text-[105px] leading-[75px] xl:leading-[110px]">
+						Odlewnia Artystyczna
+					</p>
+				</div>
+				<CustomButton navlink path="/realizacje" className="max-w-[240px] mt-12">
+					Zobacz realizacje
+				</CustomButton>
+			</div>
+
+			{/* MOBILE */}
+			<div
+				id="home"
+				className="h-customScreen p-5 dp:px-10 pb-28 flex flex-col justify-end dp:w-1/2 dp:justify-end dp:items-start gap-4 homepage-background scroll-mt-header"
 			>
-				Zobacz realizacje
-			</CustomButton>
+				<CompanyName mode="light" className="dp:hidden" />
+				<CustomButton navlink path="/realizacje" className="dp:hidden">
+					Zobacz realizacje
+				</CustomButton>
+			</div>
 		</div>
 	)
 }
