c7c4f57f7e5354965cdeedaa43c6f473bc18f03f
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-12-03 15:57:08 +0100

    Update: Change paths in MenuContent in index.ts from constants

--- src/components/layout/menu/MenuHeader.tsx
+++ src/components/layout/menu/MenuHeader.tsx
@@ -1,9 +1,8 @@
 'use client'
-
 import { MenuContent } from '@/lib/constants'
 import { capitalizeEntireText, capitalizeFirstLetter, cn } from '@/lib/utils'
 import Link from 'next/link'
-import { useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 type MenuHeaderProps = {
 	capitalize?: 'entire' | 'first' | 'default'
@@ -11,22 +10,18 @@ type MenuHeaderProps = {
 }
 
 export default function MenuHeader({ className, capitalize }: MenuHeaderProps) {
-	const [activeTabIndex, setActiveTabIndex] = useState<number>(0)
+	const path = usePathname()
 
-	const handleTabClick = (index: number) => () => {
-		setActiveTabIndex(index)
-	}
 	return (
 		<div className="hidden dp:block">
 			<nav className="w-full">
 				<ul className={cn('flex justify-end items-center', className)}>
-					{MenuContent.map((item, index) => (
+					{MenuContent.map((item) => (
 						<Link href={item.path} key={item.label}>
 							<li
-								onClick={handleTabClick(index)}
 								className={cn(
 									'min-w-[120px] xl:max-w-[190px] xl:min-w-[170px] py-2 flex justify-center items-center border-b-[1px] border-zinc-700',
-									activeTabIndex === index && 'border-accent'
+									path === item.path ? 'border-accent' : ''
 								)}
 							>
 								<p className="text-textLight text-[17px]">
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,5 +1,5 @@
 import { Menu } from '@/lib/constants/texts'
-import { Routes, HomePageSections } from '@/lib/constants/paths'
+import { Routes } from '@/lib/constants/paths'
 
 export const MenuFooterContent = [
   {label: Menu.HOME, path: Routes.HOME },
@@ -11,11 +11,11 @@ export const MenuFooterContent = [
 ]
 
 export const MenuContent = [
-  {label: Menu.HOME, path: HomePageSections.HOME },
-  {label: Menu.ABOUT, path: HomePageSections.ABOUT },
-  {label: Menu.OFFER, path: HomePageSections.OFFER },
-  {label: Menu.REALIZATIONS, path: HomePageSections.REALIZATIONS },
-  {label: Menu.CONTACT, path: HomePageSections.CONTACT },
+  {label: Menu.HOME, path: Routes.HOME },
+  {label: Menu.ABOUT, path: Routes.ABOUT },
+  {label: Menu.OFFER, path: Routes.OFFER },
+  {label: Menu.REALIZATIONS, path: Routes.REALIZATIONS },
+  {label: Menu.CONTACT, path: Routes.CONTACT },
 ]
 
 export const Opinions = [
