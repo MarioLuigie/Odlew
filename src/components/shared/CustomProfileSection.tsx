df0b0f6823f40fe03bdf8c1143374f821b2dd07e
Mariusz Lotocki <mk.lotocki@gmail.com>
2024-12-13 12:38:15 +0100

    Styles: Add object cover for image in CustomProfileSection.tsx

--- src/components/shared/CustomProfileSection.tsx
+++ src/components/shared/CustomProfileSection.tsx
@@ -14,19 +14,26 @@ export default function CustomProfileSection({
 	className,
 }: CustomProfileSectionProps) {
 	return (
-		<div className={cn('bg-yellow-400 dp:h-full flex flex-col pb-8 dp:pb-0 dp:flex-row dp:border-b-[1px] border-zinc-400', className)}>
+		<div
+			className={cn(
+				'dp:h-full flex flex-col pb-8 dp:pb-0 dp:flex-row dp:border-b-[1px] border-zinc-400',
+				className
+			)}
+		>
 			{picture && (
-				<div className='dp:w-1/2 dp:h-full dp:bg-red-500'>
+				<div className="dp:w-1/2 dp:h-full">
 					<Image
 						src={picture.path}
 						alt={picture.alt}
 						width={2400}
 						height={2400}
-						className='dp:object-fill mt-4 dp:mt-0'
+						className="dp:object-cover dp:h-full mt-4 dp:mt-0"
 					/>
 				</div>
 			)}
-			<div className='dp:w-1/2 bg-blue-300'>{children}</div>
+			<div className="dp:w-1/2 bg-blue-300 dp:flex dp:flex-col dp:items-center dp:justify-center">
+				{children}
+			</div>
 		</div>
 	)
 }
