3e6fd360042f9a552b9cabc8a5c8c67775c5a5f6
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-02-23 13:09:07 +0100

    Styles: Change size of logo in Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,7 @@ import MenuHeader from '@/components/layout/menu/MenuHeader'
 export default function Header() {
 	return (
 		<header className="z-50 min-h-header dp:min-h-[130px] w-full sticky top-0 flex items-center justify-between px-5 py-2 bg-headerBg">
-			<Logo w={180} />
+			<Logo w={210} />
 			<MenuSheet />
 			<MenuHeader capitalize='first'/>
 		</header>
