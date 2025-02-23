f954d30e453eca68eb4140cefe3f50f8d18db64d
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-02-23 14:17:41 +0100

    Styles: Add bg-transparent for Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import MenuHeader from '@/components/layout/menu/MenuHeader'
 
 export default function Header() {
 	return (
-		<header className="z-50 min-h-header dp:min-h-[130px] w-full sticky top-0 flex items-center justify-between px-5 py-2 bg-headerBg">
+		<header className="z-50 min-h-header dp:min-h-[130px] w-full sticky top-0 flex items-center justify-between px-5 py-2 bg-transparent">
 			<Logo w={210} />
 			<MenuSheet />
 			<MenuHeader capitalize='first'/>
