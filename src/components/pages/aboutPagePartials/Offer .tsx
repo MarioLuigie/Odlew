f9ed39f1c837d308345ce2d3597ca725fdee2eeb
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-12 21:48:25 +0200

    Styles: Create new composition for elements in Offer.tsx

--- src/components/pages/aboutPagePartials/Offer .tsx
+++ src/components/pages/aboutPagePartials/Offer .tsx
@@ -8,42 +8,42 @@ import ImagesVerticalLine from '@/components/shared/ImagesVerticalLine'
 
 export default function Offer() {
 	return (
-		<section
-			id="oferta"
-			className="bg-background pt-24 scroll-mt-header"
-		>
+		<section id="oferta" className="bg-background pt-24 scroll-mt-header">
 			<div className="lg:flex lg:flex-col md:min-h-customScreen">
 				{/* TOP SECTION */}
 				<div className="lg:flex lg:min-h-[820px] pb-10">
 					{/* TEXT CONTAINER*/}
-					<div className="w-7/12">
-						<p className="text-[54px] font-normal text-[#383638]">
-							Co oferujemy i dla kogo?
-						</p>
-						<p className="text-lg font-medium text-[#383638]">
-							Współpracujemy z artystami, którzy swoje prace tworzą w
-							plastycznym materiale tj. wosk, glina, plastelina lub
-							drewno, a chcą je utrwalić w metalowej postaci. Z każdym
-							kto chce mieć coś stworzonego - “wyrzeźbionego” przez
-							siebie. Stworzyć oryginalny prezent dla bliskiej osoby. Do
-							współpracy zachęcamy instytucje kultury, urzędy i
-							organizacje, chcące upamiętnić ważne wydarzenia lub osoby.
-						</p>
-						<div className="px-5 my-12">
-							<CustomButton navlink path="/oferta">
-								Czytaj więcej
-							</CustomButton>
+					<div className="lg:w-6/12 xl:w-7/12 pr-16 lg:pl-[60px] xl:pl-[135px]">
+						<div className='flex flex-col justify-center h-full'>
+							<p className="text-[50px] font-normal text-[#383638]">
+								Co oferujemy i dla kogo?
+							</p>
+							<p className="text-lg font-medium text-[#383638]">
+								Współpracujemy z artystami, którzy swoje prace tworzą w
+								plastycznym materiale tj. wosk, glina, plastelina lub
+								drewno, a chcą je utrwalić w metalowej postaci. Z każdym
+								kto chce mieć coś stworzonego - “wyrzeźbionego” przez
+								siebie. Stworzyć oryginalny prezent dla bliskiej osoby.
+								Do współpracy zachęcamy instytucje kultury, urzędy i
+								organizacje, chcące upamiętnić ważne wydarzenia lub
+								osoby.
+							</p>
+							<div className="px-5 my-12">
+								<CustomButton navlink path="/oferta">
+									Czytaj więcej
+								</CustomButton>
+							</div>
 						</div>
 					</div>
 
 					{/* IMAGES CONTAINER*/}
-					<div className='bg-yellow-400 grow'>
+					<div className="grow">
 						<ImagesVerticalLine>dffghnghnnt</ImagesVerticalLine>
 					</div>
 				</div>
 
 				{/* BOTTOM SECTION */}
-				<div className="flex gap-2 overflow-x-auto">
+				<div className="lg:pr-[120px] xl:pr-[300px]">
 					{HomeOfferImages.map((image, i) => (
 						<Image
 							key={i}
@@ -52,7 +52,7 @@ export default function Offer() {
 							width={2400}
 							height={2400}
 							priority
-							className="h-customScreen w-auto object-cover"
+							className="w-auto object-cover"
 						/>
 					))}
 				</div>
