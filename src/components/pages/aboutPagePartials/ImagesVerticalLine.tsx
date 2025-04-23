b0ca40cee7d75e0241fdc5ead045c243d3c48b1a
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-04-23 17:20:52 +0200

    Styles: Add position params for images in breakpoint 3xl in ImagesVerticalLine.tsx

--- src/components/pages/aboutPagePartials/ImagesVerticalLine.tsx
+++ src/components/pages/aboutPagePartials/ImagesVerticalLine.tsx
@@ -6,7 +6,7 @@ export default function ImagesVerticalLine() {
 		<div className="h-[685px] w-full flex">
 			<div className="w-[2px] h-full bg-[#463E46]"></div>
 			<div className="relative h-full w-full">
-				<div className="z-40 absolute bottom-[47px] left-[56px] w-[245px] h-[165px]">
+				<div className="z-40 absolute md:bottom-[47px] md:left-[56px] 3xl:left-[130px] w-[245px] h-[165px]">
 					<Image
 						src={AboutImages.offer_1.path}
 						alt={AboutImages.offer_1.alt}
@@ -14,7 +14,7 @@ export default function ImagesVerticalLine() {
 						className="w-full h-full object-cover"
 					/>
 				</div>
-				<div className="absolute top-[166px] right-[40px] w-[270px] h-[400px]">
+				<div className="absolute md:top-[166px] md:right-[40px] 3xl:right-[130px] w-[270px] h-[400px]">
 					<Image
 						src={AboutImages.offer_2.path}
 						alt={AboutImages.offer_2.alt}
